281f6fe05f75ca43f3780033087462696e83ae3e
Leonardo Lucena <82075928+leonardolucena261@users.noreply.github.com>
2025-09-10 15:01:48 -0300

    Fix Vite build target for browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    target: 'es2017'
   },
   server: {
     port: 3000
